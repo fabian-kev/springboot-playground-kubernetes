a812de9bba4a17e2adb28ef5bb25a92656eab9d4
kev <fbian.kevin@gmail.com>
2025-03-22 10:38:02 +0800

    Initial commit

---
+++ springboot-kubernetes-deployment/src/main/java/com/fabiankevin/springboot_kubernetes_deployment/SpringbootKubernetesDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.fabiankevin.springboot_kubernetes_deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKubernetesDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKubernetesDeploymentApplication.class, args);
+	}
+
+}
---
+++ springboot-kubernetes-deployment/src/test/java/com/fabiankevin/springboot_kubernetes_deployment/SpringbootKubernetesDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiankevin.springboot_kubernetes_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKubernetesDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
